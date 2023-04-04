11ebbfe03ffba8f9911e86b89eb47b4f72e182b9
NILZA777 <129033297+NILZA777@users.noreply.github.com>
2023-04-04 11:23:08 -0400

    Add files via upload

---
+++ src/pages/login.page.ts
@@ -0,0 +1,8 @@
+class LoginPage {
+    // locators
+    private username: string ='#user-name';
+    private password:string = '#password';
+    private loginButton: string = '#login-button';
+
+    constructor(){}
+}
